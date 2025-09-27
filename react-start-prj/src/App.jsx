74840d16cbaac922c9673d7c0bf180ae7f44d5cd
feverK13 <ihor.k2006@gmail.com>
2025-09-27 23:50:57 +0300

    fonts implementation tested

---
+++ react-start-prj/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='dmnApp'>
+        <h1>HI, that's my first Vite + React page)</h1>
+        <p>Wish to continue developing with that technologies</p>
+      </div>
+    </>
+  );
+}
+
+export default App
