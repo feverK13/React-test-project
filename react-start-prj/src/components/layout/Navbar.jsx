fc9f08f005e7282bb3c4ffe320d00dfe9bca863e
feverK13 <ihor.k2006@gmail.com>
2025-10-31 00:07:14 +0200

    created working and styled layout

--- react-start-prj/src/components/layout/Navbar.jsx
+++ react-start-prj/src/components/layout/Navbar.jsx
@@ -1,43 +1,43 @@
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
-
+import styles from '../../styles/Navbar.module.css'
 
 export default function Navbar() {
   const {isLoggedIn, logout} = useAuth()
   const getNavLinkClass = ({isActive}) => {
-    return isActive ? 'navLink active' : 'navLink'
+    return isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
   }
 
   return (
-    <nav className='navBar'>
-        <NavLink to='/' className='navLogo'>
+    <nav className={styles.navBar}>
+        <NavLink to='/' className={styles.navLogo}>
           Arete
         </NavLink>
 
-        <ul className='navList'>
+        <ul className={styles.navList}>
           {isLoggedIn && (
             <>
-              <li className='listItem'>
+              <li className={styles.listItem}>
                 <NavLink to='/' className={getNavLinkClass}>Головна</NavLink>
               </li>
-              <li className='listItem'>
+              <li className={styles.listItem}>
                 <NavLink to='/shop' className={getNavLinkClass}>Магазин</NavLink>
               </li>
             </>
           )}
 
-          <li className='listItem'>
+          <li className={styles.listItem}>
             <NavLink to='/about-us' className={getNavLinkClass}>Про Нас</NavLink>
           </li>
 
           {!isLoggedIn && (
-            <li className='listItem loginLink'>
-              <NavLink to='/auth' className={getNavLinkClass}>Увійти</NavLink>
+            <li className={styles.listItem}>
+              <NavLink to='/auth' className={`${getNavLinkClass} ${styles.loginLink}`}>Увійти</NavLink>
             </li>
           )}
 
           {isLoggedIn && (
-            <li className='listItem exitLink'>
+            <li className={`${styles.listItem} ${styles.exitBut}`}>
               <button onClick={logout} className='logoutBut'>Вихід</button>
             </li>
           )}
--- react-start-prj/src/main.jsx
+++ react-start-prj/src/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import AuthProvider from './context/AuthContext'
-import './styles/index.css'
 import App from './App.jsx'
+
+import './styles/reset.css'
+import './styles/index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
