0c4d1f50fb48a270f5c98a686719951a16f18c26
feverK13 <ihor.k2006@gmail.com>
2026-01-16 23:32:47 +0200

    created burger menu on mobile version, also fixed displaying buttons

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import { useAuth } from '../../context/AuthContext'
+import Button from '../ui/Button.jsx'
 import styles from './Navbar.module.css'
+
+function BurgerIcon({ isOpen, onClick }) {
+  return (
+    <div onClick={onClick} className={styles.burgerIcon}>
+      {isOpen ? (
+        <svg xmlns='http://www.w3.org/2000/svg' width={40} height={40} viewBox='0 0 24 24'>
+          <path
+            fill='#131313'
+            d='M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6z'
+          ></path>
+        </svg>
+      ) : (
+        <svg xmlns='http://www.w3.org/2000/svg' width={40} height={40} viewBox='0 0 24 24'>
+          <path fill='#131313' d='M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z'></path>
+        </svg>
+      )}
+    </div>
+  )
+}
 
 export default function Navbar() {
   const { isLoggedIn, logout } = useAuth()
@@ -9,13 +30,25 @@ export default function Navbar() {
     return isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
   }
 
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(prev => !prev)
+  }
+
+  // const closeMenu = () => {
+  //   setIsOpen(false)
+  // }
+
   return (
     <nav className={styles.navBar}>
       <NavLink to='/' className={styles.navLogo}>
         Arete
       </NavLink>
 
-      <ul className={styles.navList}>
+      <BurgerIcon onClick={toggleMenu} isOpen={isOpen} />
+
+      <ul className={isOpen ? styles.navListMenu : styles.navList}>
         {isLoggedIn && (
           <>
             <li className={styles.listItem}>
@@ -46,10 +79,8 @@ export default function Navbar() {
         )}
 
         {isLoggedIn && (
-          <li className={`${styles.listItem} ${styles.exitBut}`}>
-            <button onClick={logout} className='logoutBut'>
-              Вихід
-            </button>
+          <li>
+            <Button onClick={logout} children='Вихід' variant='navItem' />
           </li>
         )}
       </ul>
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,6 +4,8 @@ export default function Button({ className, variant, children, ...props }) {
   let buttonStyles = `${styles.button} ${className || ''}`
   if (variant === 'outline') {
     buttonStyles += ` ${styles.outlineButton}`
+  } else if (variant === 'navItem') {
+    buttonStyles = ` ${styles.navItemButton}`
   }
   return (
     <button className={buttonStyles} {...props}>
